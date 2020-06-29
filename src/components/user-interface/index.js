f2bff4ad2d5fb91b82025d01bcacb0534c8b6e38
Corné Botma <cornebotma@hotmail.com>
2020-06-29 18:25:37 +0200

    Added profile images

--- src/components/user-interface/index.js
+++ src/components/user-interface/index.js
@@ -22,8 +22,9 @@ class PersonList extends React.Component {
         <div className="row">
           {users.map(user =>
             <div className="col-lg-3 col-md-4 user-layout__col" key={user.id}>
-              <div className="inner">
-                {user.name}
+              <div className="user-layout__col--inner">
+                <img src={'https://i.pravatar.cc/100?img=' + user.id}></img>
+                <p className="font-weight-bold">{user.name}</p>
                 </div>
             </div>
           )}
